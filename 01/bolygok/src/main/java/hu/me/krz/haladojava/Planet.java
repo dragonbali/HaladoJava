673076771e67b89dad7b3eaf6891923e70c918f3
Hornyák Balázs <kicsikis@gmail.com>
2020-02-12 19:26:07 +0100

    Ez az ameddig eljutottam órán. Tehát a 2. feladat végéig. (remélhetoleg eddig helyes minden)

---
+++ 01/bolygok/src/main/java/hu/me/krz/haladojava/Planet.java
@@ -0,0 +1,28 @@
+package hu.me.krz.haladojava;
+
+public final class Planet {
+
+	private final String position;
+	private final int radius;
+	private final String name;
+	
+	public Planet(String position, int radius, String name) {
+		super();
+		this.position = position;
+		this.radius = radius;
+		this.name = name;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public int getRadius() {
+		return radius;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+}
