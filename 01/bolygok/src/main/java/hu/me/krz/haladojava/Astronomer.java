e3f5e41a934b01e9685ecd8587abbba1c786f4b0
hornyak8 <hornyak8@iit342.iit.uni-miskolc.hu>
2020-02-19 15:48:23 +0100

    elvileg kesz de nem jo igazabol

--- 01/bolygok/src/main/java/hu/me/krz/haladojava/Astronomer.java
+++ 01/bolygok/src/main/java/hu/me/krz/haladojava/Astronomer.java
@@ -1,20 +1,32 @@
 package hu.me.krz.haladojava;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Astronomer {
-
 	private String name;
-	private List<Planet> DiscoveredPlanets = new ArrayList<>();
-	
+	private List<Planet> discoveredPlanets;
 	public Astronomer(String name) {
 		super();
 		this.name = name;
+		discoveredPlanets = new ArrayList<>();
 	}
-
-	public List<Planet> getDiscoveredPlanets() {
-		return DiscoveredPlanets;
+	public String getName() {
+		return name;
 	}
 	
+	public List<Planet> getDiscoveredPlanets() {
+		return discoveredPlanets;
+	}
+
+	public void observeTheSky() {
+		for (int i = 0; i < 10; i++) {
+			double radian = new Random().nextInt(1000000);
+			String name = this.name + "_" + (i);
+			Point position = new Point((i + 1) * 10, 0, 0);		
+			discoveredPlanets.add(new Planet(pos, rad, name));
+		}
+	}
 	
 }
