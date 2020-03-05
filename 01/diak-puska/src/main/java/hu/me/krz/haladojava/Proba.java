3e5a9d31cad732f8a68f0767aa6dcb1c68fa11f2
Hornyák Balázs <kicsikis@gmail.com>
2020-03-05 17:49:04 +0100

    Diak puska kesz, de a 3.feladatban levo reszt nem ertem. futtatasnal azt mondja nem adtam at parametert, ami ertheto mert nem is tudom hogy kene neki kapnia akarmit is.

--- 01/diak-puska/src/main/java/hu/me/krz/haladojava/Proba.java
+++ 01/diak-puska/src/main/java/hu/me/krz/haladojava/Proba.java
@@ -1,13 +1,16 @@
 package hu.me.krz.haladojava;
 
-public class Proba {
+import java.util.Random;
 
+public class Proba {
+	
 	public static void main(String[] args) {
+		
 		if (args.length > 1) {
 			System.err.println("Tobb parametert adott at!");
 			System.exit(-1);
 		}
-		
+	
 		int parameter;
 		
 		try {
@@ -15,11 +18,36 @@ public class Proba {
 			
 			Diak[] diakokTomb = new Diak[parameter];
 			for (int i = 0; i < diakokTomb.length; i++) {
-				
+				diakokTomb[i] = new Diak("diak" + i + 1 , new Random().nextInt(8) + 11, new Random().nextInt(6));
 			}
+			
+			sortByPuska(diakokTomb);
+			
+			for (int i = 0; i < diakokTomb.length; i++) {
+				System.out.println(diakokTomb[i].toString());
+			}
+							
 		} catch (Exception e) {
-			// TODO: handle exception
+			System.out.println(e.getMessage());
 		}
+		
+	} 
+
+	private static Diak[] sortByPuska(Diak[] diakokTomb) {
+		int swap;
+		do {
+			swap = 0;
+			for (int i = 0; i < diakokTomb.length; i++) {
+				if (diakokTomb[i].compareTo(diakokTomb[i + 1]) == -1 ) {
+					Diak temp = diakokTomb[i];
+					diakokTomb[i] = diakokTomb[i + 1];
+					diakokTomb[i + 1] = temp;
+					swap++;
+				}
+			}
+		} while (swap == 0);
+		
+		return diakokTomb;
 	}
 
 }
