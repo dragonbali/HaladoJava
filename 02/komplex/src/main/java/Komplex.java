f517dd99b7e9807fdfe44aaa59596d5c0ba76d7e
Hornyák Balázs <kicsikis@gmail.com>
2020-03-30 15:03:12 +0200

    bemasolva a Tanar ur komplex feladata

---
+++ 02/komplex/src/main/java/Komplex.java
@@ -0,0 +1,19 @@
+//import sun.security.krb5.internal.ccache.CredentialsCache;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Komplex {
+
+    public static void main(String[] args) {
+        UseRepository useRepository = new UseRepository();
+        List<Validator> validators = new ArrayList<>();
+        validators.add(new LengthValidator());
+        validators.add(new SpaceValidator());
+        UserService userService = new UserServiceImpl(useRepository, validators);
+        UserController userController = new UserController(userService, new NeptunCodeGenerator());
+        userController.save(new UserDto("Van Benne Space"));
+        userController.save(new UserDto("Rovid"));
+        userController.save(new UserDto("Nagyonjo"));
+    }
+}
